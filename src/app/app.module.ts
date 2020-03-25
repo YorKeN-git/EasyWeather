609378dc521ba18287132825be91485401d3cadb
YorKeN <dethoor.axel@gmail.com>
2020-03-25 22:30:57 +0100

    WIP : http get API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ResultatComponent } from './pages/resultat/resultat.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { ResultatComponent } from './pages/resultat/resultat.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
     AppRoutingModule
